6fa46bcc4c3106a1460f7c61e585fcfc16e159a2
bezumkin <bezumkin@yandex.ru>
2013-08-14 22:49:19 +0700

    Improved pdoTools installation

--- _build/build.config.php
+++ _build/build.config.php
@@ -9,7 +9,13 @@ define('PKG_RELEASE', 'pl3');
 
 
 /* define paths */
-define('MODX_BASE_PATH', dirname(dirname(dirname(__FILE__))) . '/');
+if (is_file(dirname(dirname(dirname(__FILE__))))) {
+	define('MODX_BASE_PATH', dirname(dirname(dirname(__FILE__))) . '/');
+}
+else {
+	define('MODX_BASE_PATH', dirname(dirname(dirname(dirname(__FILE__)))) . '/');
+}
+
 define('MODX_CORE_PATH', MODX_BASE_PATH . 'core/');
 define('MODX_MANAGER_PATH', MODX_BASE_PATH . 'manager/');
 define('MODX_CONNECTORS_PATH', MODX_BASE_PATH . 'connectors/');
